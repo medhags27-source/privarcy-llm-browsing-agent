7c0454e8de3a7d86bc7f407063aeeab44d783a0e
meda <med@gmail.com>
2026-09-22 22:26:25 +0530

    Connect Chrome extension to FastAPI

--- extension/background.js
+++ extension/background.js
@@ -2,12 +2,33 @@ console.log("Privacy Agent background service worker started");
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.type === "PAGE_DATA") {
-    console.log("Page data received from content script:");
-    console.log("Title:", message.data.title);
-    console.log("Content length:", message.data.content.length);
+    console.log("Sending page data to FastAPI...");
 
-    sendResponse({
-      status: "received"
-    });
+    fetch("http://127.0.0.1:8000/api/capture", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        content: message.data.content
+      })
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("FastAPI response:", data);
+        sendResponse({
+          status: "success",
+          backendResponse: data
+        });
+      })
+      .catch((error) => {
+        console.error("Error sending data to FastAPI:", error);
+        sendResponse({
+          status: "error",
+          message: error.message
+        });
+      });
+
+    return true;
   }
 });
